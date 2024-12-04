82e368b66536aeaae99cdd3d64c3c6dce3b08062
achraf.hidine <achraf.hidine@cdbdx.biz>
2024-12-04 23:45:13 +0100

    update routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,11 +4,30 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../features/Home";
 import Play from "../features/Play";
 import Dashboard from "../features/Dashboard";
-import ProtectedRoute from "./ProtectedRoute";
 import Auth from "../features/Auth";
+import { loadSession } from "../store/slices/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import ProtectedRoute from "./ProtectedRoute";
 import NotFound from "../features/NotFound";
+import { AppDispatch, RootState } from "../store";
+import { fetchTreasures } from "../store/slices/treasuresSlice";
+import LoadingPage from "../components/Loading";
 
 const AppRoutes: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { status, treasures } = useSelector(
+    (state: RootState) => state.treasures
+  );
+
+  useEffect(() => {
+    dispatch(loadSession());
+    if (treasures.length === 0) dispatch(fetchTreasures());
+  }, [dispatch, treasures.length]);
+
+  if (status === "loading") {
+    return <LoadingPage />;
+  }
+
   return (
     <Router>
       <MainLayout>
